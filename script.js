a234ef60853d97eb0c70ed13cbb9705760a446a5
Ashwini277 <devarushiashwini@gmail.com>
2024-08-01 20:00:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+describe('Holiday Destination Webpage Tests', () => {
+    it('Should find the heading tag', () => cy.get('h1').should('be.visible'));
+    
+    it('Should find the paragraph tag', () => cy.get('p').should('be.visible'));
+    
+    it('Should find the ordered list tag', () => cy.get('ol').should('be.visible'));
+    
+    it('Should find the unordered list tag', () => cy.get('ul').should('be.visible'));
+    
+    it('Should find the strong tag', () => cy.get('strong').should('be.visible'));
+    
+    it('Should find the em tag', () => cy.get('em').should('be.visible'));
+});
+
